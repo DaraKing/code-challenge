30f86a33876639b37dc896e35e3271884876a033
dario <dario.knezovich@gmail.com>
2021-04-20 19:40:20 +0200

    Header set.

---
+++ src/components/partials/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <img alt="DEPT logo" src="../../assets/logo.svg">
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+  border-bottom: 1px solid #000000;
+}
+</style>
