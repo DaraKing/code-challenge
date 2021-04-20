cfa6253cf3d70b4d529c61e692293263df6de854
dario <dario.knezovich@gmail.com>
2021-04-20 19:40:58 +0200

    Axios setup and first fetch.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
   <div id="app">
-    <img alt="DEPT logo" src="./assets/logo.svg">
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "@/components/partials/Header";
+import constants from "@/constants";
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {Header},
+  data() {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    fetchUsers() {
+      this.axios.get(constants.API_URL).then((response) => {
+        this.users = response.data.results ?? []
+      })
+    }
+  },
+  created() {
+    this.fetchUsers()
+  }
 }
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  width: 100%;
 }
 </style>
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const constants = {
+    API_URL: 'https://randomuser.me/api/?seed=dept&results=15&dob=18'
+};
+
+export default Object.freeze(constants);
