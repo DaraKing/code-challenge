4bb9c4d1bef20b22d48614688210eea6b105d42c
dario <dario.knezovich@gmail.com>
2021-04-20 20:45:05 +0200

    Made reusable Icon component.

---
+++ src/components/reusable/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :alt="name" :src="require(`../../assets/${name}.svg`)">
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
